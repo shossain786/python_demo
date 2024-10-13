f0c0ac859790e9b98fefd13a943810a8a8545861
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-10-13 17:54:11 +0530

    problem excercise solved

--- src/main/org.saddy/python_core/loops_control/excercise_problems.py
+++ src/main/org.saddy/python_core/loops_control/excercise_problems.py
@@ -12,7 +12,26 @@ def count_alphabets_and_digits(input_string):
 
     return alphabet_count, digit_count
 
-text = 'Pune-411048'
-alphabets, digits = count_alphabets_and_digits(text)
-print("Number of alphabets:", alphabets)
-print("Number of digits:", digits)
+#! d: Enter 5 digit number through keyboard and reverse the number and check whether the number is palindrome or not.
+def palindromeNumber():
+    number = int(input('Enter a number:'))
+    print('You have entered: ', number)
+    original_num = number
+    revised_num = 0
+    while number > 0:
+        revised_num  = 10*revised_num + number % 10 
+        number = number // 10
+    print('Revised Number: ', revised_num)
+    if original_num == revised_num:
+        print(original_num, 'is Palindrome')
+    else:
+        print(original_num, 'is not Palindrome')
+
+def main():
+    # alphabets, digits = count_alphabets_and_digits('Pune-411048')
+    # print("Number of alphabets:", alphabets)
+    # print("Number of digits:", digits)
+    palindromeNumber()
+
+if __name__ == "__main__":
+    main()
